94ce1aa40e92dfe223bb7e55f7b8ee21e32bbfc8
nikfior <61381998+nikfior@users.noreply.github.com>
2023-11-11 07:52:46 +0200

    UI changes in the menu and its list changed the name of the List component to MyList
    also updated browserslist

--- src/MainView.js
+++ src/MainView.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
-import List from "./List";
-import Alert from "./Alert";
+import MyList from "./MyList";
 import getDataCheckSession from "./getDataCheckSession";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 //
 
 const MainView = () => {
   const navigate = useNavigate();
   const [name, setName] = useState("");
-  const [list, setList] = useState(null);
+  // listAll holds all the items
   const [listAll, setListAll] = useState(null);
+  // list holds only the viewable items. Some may have been temporarily removed (hidden) during the use of the search field
+  const [list, setList] = useState(null);
   const [username, setUsername] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [placeholder, setPlaceholder] = useState("url");
   const [queries, setQueries] = useState({});
   const [slowCrawl, setSlowCrawl] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({ msg: "", type: "" });
+  const [openSnackBar, setOpenSnackBar] = React.useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name && !isEditing) {
-      showAlert(true, "danger", "please enter a site url");
+      showAlert("warning", "please enter a site url");
     } else if (isEditing) {
       const tempQueries = { ...queries };
       tempQueries[editID] = name.toString();
@@ -32,7 +40,7 @@ const MainView = () => {
       setEditID(null);
       setIsEditing(false);
       setPlaceholder("url");
-      showAlert(true, "success", "query parameters added for future analysis");
+      showAlert("success", "query parameters added for future analysis");
     } else {
       // add new site for scraping
 
@@ -40,14 +48,15 @@ const MainView = () => {
       getDataCheckSession(`${process.env.REACT_APP_BACKEND}api/sites/`, "POST", { url: name, slowCrawl })
         .then((data) => {
           // showAlert(true, "danger", `${data.msg}`);
-          const alertType = data.msg.startsWith("The site is being scraped") ? "success" : "danger";
-          showAlert(true, alertType, data.msg);
+          const alertType = data.msg.startsWith("The site is being scraped") ? "success" : "error";
+          showAlert(alertType, data.msg);
           // setList([...list, newItem]);
           setName("");
-          // show site list because onChange doesn't work if i change value programmatically
+          // I reset the viewable list to the listAll that has all the items because they got reduced due to the search field
           if (listAll) {
             setList(listAll);
           }
+          refreshListItems();
         })
         .catch((error) => {
           if (error.message === "Credentials missing") {
@@ -59,21 +68,66 @@ const MainView = () => {
     }
   };
 
-  const showAlert = (show = false, type = "", msg = "") => {
-    setAlert({ show, type, msg });
+  const showAlert = (type = "info", msg = "") => {
+    setAlert({ type, msg });
+    setOpenSnackBar(true);
   };
 
   const clearList = () => {
-    showAlert(true, "danger", "empty list");
+    showAlert("warning", "empty list");
     setList([]);
   };
 
-  const removeItem = (id) => {
-    getDataCheckSession(`${process.env.REACT_APP_BACKEND}api/sites/${id}`, "DELETE")
+  const removeItem = (id, savedAnalysisId) => {
+    console.log(id, savedAnalysisId);
+    getDataCheckSession(
+      `${process.env.REACT_APP_BACKEND}api/sites/${id}${
+        savedAnalysisId ? "?savedanalysisid=" + savedAnalysisId : ""
+      }`,
+      "DELETE"
+    ) // TODO better checks and remove correctly
       .then((data) => {
-        showAlert(true, "danger", `${data.msg}`);
-        setList(list.filter((item) => item.id !== id));
-        setListAll(listAll.filter((item) => item.id !== id));
+        if (
+          data.msg === "Analysis deleted successfully" ||
+          data.msg === "Site and analyses deleted successfully"
+        ) {
+          showAlert("success", `${data.msg}`);
+          refreshListItems();
+          //setList(list.filter((item) => item.id !== id));
+          //setListAll(listAll.filter((item) => item.id !== id));
+        } else {
+          showAlert("error", `Deletion failed. ${data.msg}`);
+        }
+      })
+      .catch((error) => {
+        if (error.message === "Credentials missing") {
+          navigate("/");
+        } else {
+          console.log(error.name + ": " + error.message);
+        }
+      });
+  };
+
+  // gets the items from the backend. Can be called when I want to refresh the items in the list because I added or deleted one
+  const refreshListItems = () => {
+    getDataCheckSession(`${process.env.REACT_APP_BACKEND}api/sites/getMenu`)
+      .then((data) => {
+        const tempusername = data.username ? data.username : "";
+        setUsername(tempusername);
+
+        const tempList = data.sites.map((site) => {
+          return {
+            url: site.url,
+            id: site.id,
+            parameters: site.parameters,
+            scrapeStatus: site.scrapeStatus,
+            analyses: site.analyses,
+            fetchedFavicon: `${new URL(site.url).origin}/favicon.ico`,
+          };
+        });
+
+        setList(tempList);
+        setListAll(tempList);
       })
       .catch((error) => {
         if (error.message === "Credentials missing") {
@@ -94,32 +148,7 @@ const MainView = () => {
   };
 
   useEffect(() => {
-    getDataCheckSession(`${process.env.REACT_APP_BACKEND}api/sites/getMenu`)
-      .then((data) => {
-        const tempusername = data.username ? data.username : "";
-        setUsername(tempusername);
-
-        const tempList = data.sites.map((site) => {
-          return {
-            url: site.url,
-            id: site.id,
-            parameters: site.parameters,
-            scrapeStatus: site.scrapeStatus,
-            status: site.status,
-            analyses: site.analyses,
-          };
-        });
-
-        setList(tempList);
-        setListAll(tempList);
-      })
-      .catch((error) => {
-        if (error.message === "Credentials missing") {
-          navigate("/");
-        } else {
-          console.log(error.name + ": " + error.message);
-        }
-      });
+    refreshListItems();
 
     //
   }, []);
@@ -130,7 +159,6 @@ const MainView = () => {
     <div>
       <section className="section-center">
         <form className="sites-form" onSubmit={handleSubmit}>
-          {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
           <h4 style={{ marginBottom: "1.75rem", textTransform: "initial" }}>Hello {username}</h4>
           <h3>Site List</h3>
 
@@ -182,7 +210,12 @@ const MainView = () => {
 
         {list !== null ? (
           <div className="sites-container">
-            <List items={list} queries={queries} removeItem={removeItem} editItem={editItem} />
+            <MyList
+              items={list}
+              removeItem={removeItem}
+              showAlert={showAlert}
+              refreshListItems={refreshListItems}
+            />
             <button className="clear-btn" onClick={clearList} disabled style={{ color: "gray" }}>
               clear items
             </button>
@@ -193,6 +226,23 @@ const MainView = () => {
           </div>
         )}
       </section>
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={3500}
+        onClose={() => {
+          setOpenSnackBar(false);
+        }}
+      >
+        <Alert
+          onClose={() => {
+            setOpenSnackBar(false);
+          }}
+          severity={alert.type}
+          sx={{ width: "100%" }}
+        >
+          {alert.msg}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
---
+++ src/MyList.js
@@ -0,0 +1,332 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import getDataCheckSession from "./getDataCheckSession";
+import { useNavigate } from "react-router-dom";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import ListItemButton from "@mui/material/ListItemButton";
+import Collapse from "@mui/material/Collapse";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import StarBorder from "@mui/icons-material/StarBorder";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+const MyList = ({ items, removeItem, showAlert, refreshListItems }) => {
+  //
+
+  const [statusRefresher, setStatusRefresher] = useState(-1);
+  const [openCollapse, setOpenCollapse] = React.useState({});
+  const [openForm, setOpenForm] = React.useState(false);
+  const [formSiteId, setFormSiteId] = React.useState(-1);
+
+  const handleClickOpenForm = (id) => {
+    setFormSiteId(id);
+    setOpenForm(true);
+  };
+
+  const handleCloseForm = (DoIanalyze) => {
+    if (DoIanalyze) {
+      const queries = document.getElementById("nameFormQueries").value;
+      //console.log(queries);// TODO better parsing for queries. add them differently
+      getDataCheckSession(
+        `${process.env.REACT_APP_BACKEND}api/sites/analysis?id=${formSiteId}${queries || ""}`
+      )
+        .then((data) => {
+          if (data.status.startsWith("Analyzing...")) {
+            showAlert("success", "Analyzing...");
+            refreshListItems();
+          } else {
+            showAlert("error", "Something went wrong.");
+          }
+        })
+        .catch((error) => {
+          if (error.message === "Credentials missing") {
+            navigate("/");
+          } else {
+            console.log(error.name + ": " + error.message);
+          }
+        });
+    }
+
+    setFormSiteId(-1);
+    setOpenForm(false);
+  };
+
+  useEffect(() => {
+    /*
+    console.log("In useffect");
+    const refreshTimer = setInterval(() => {
+      console.log("beep " + statusRefresher);
+    }, 4000);
+    setStatusRefresher(refreshTimer);
+
+    //console.log("IN EFFECT " + statusRefresher);
+    //
+    return () => console.log("RETURNSS");
+
+    */
+  }, []);
+
+  //console.log("OUTSIDE EFFECT " + statusRefresher);
+
+  // useEffect(() => {
+  //   if (openCollapse.length !== items.length) {
+  //     const tempArr = new Array(items.length);
+  //     for (let i = 0; i < tempArr.length; i++) {
+  //       tempArr[i] = openCollapse[i] ?? false;
+  //     }
+  //     setOpenCollapse(tempArr);
+  //   }
+  // }, [items]);
+
+  let navigate = useNavigate();
+
+  return (
+    <>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {items.map((item, index) => {
+          const { id, url, scrapeStatus, analyses, fetchedFavicon } = item;
+          return (
+            <React.Fragment key={id}>
+              <ListItem
+                style={{ paddingRight: "145px" }}
+                alignItems="flex-start"
+                secondaryAction={
+                  <div style={{ display: "flex", flexDirection: "row", flexWrap: "nowrap" }}>
+                    <IconButton
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleClickOpenForm(id);
+                      }}
+                      edge="end"
+                      aria-label="delete"
+                      style={{ marginRight: "0px" }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <ListItemButton
+                      sx={{ flexGrow: 0 }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setOpenCollapse({ ...openCollapse, [id]: !openCollapse[id] });
+                      }}
+                    >
+                      {openCollapse[id] ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <IconButton
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeItem(id);
+                      }}
+                      edge="end"
+                      aria-label="delete"
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                }
+              >
+                <ListItemButton style={{ maxWidth: "100%", paddingRight: "16px" }}>
+                  <ListItemAvatar>
+                    <Avatar alt={url.split(/https?:\/\/(?:www\.)?/)[1].toUpperCase()} src={fetchedFavicon} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primaryTypographyProps={{
+                      textOverflow: "ellipsis",
+                      overflow: "hidden",
+                      whiteSpace: "nowrap",
+                      title: url,
+                    }}
+                    primary={url}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          Status:
+                        </Typography>
+                        <span
+                          style={{
+                            color: scrapeStatus === "Completed scraping Ok" ? "green" : "red",
+                          }}
+                        >
+                          {" " + scrapeStatus}
+                        </span>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+              <Collapse in={openCollapse[id]} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {analyses.map((analysis) => {
+                    // console.log(analysis);
+                    return (
+                      <ListItem
+                        key={analysis.savedAnalysisId}
+                        sx={{ pl: 4 }}
+                        secondaryAction={
+                          <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            onClick={(e) => {
+                              removeItem(id, analysis.savedAnalysisId);
+                            }}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        }
+                      >
+                        <ListItemButton
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            // clearInterval(statusRefresher);
+                            console.log(`/analysis?id=${id}&savedanalysisid=${analysis.savedAnalysisId}`);
+                            //navigate(`/analysis?id=${id}${queries[id] || ""}`);
+                          }}
+                        >
+                          {/* <ListItemText primary={analysis.savedAnalysisId} /> */}
+                          <ListItemText>{analysis.analysisStatus}</ListItemText>
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Collapse>
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          );
+        })}
+      </List>
+      <Dialog open={openForm} onClose={handleCloseForm}>
+        <DialogTitle>Analyze</DialogTitle>
+        <DialogContent>
+          <DialogContentText whiteSpace={"pre-wrap"}>
+            Please enter the analysis parameters you wish to use for the site analysis.{"\n"}
+            The available parameters can be seen in the{" "}
+            <a href={`${process.env.REACT_APP_BACKEND}api-docs/`} target="_blank" rel="noreferrer">
+              Documentation Page
+            </a>
+            .
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="nameFormQueries"
+            label="Parameters"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              handleCloseForm(false);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              handleCloseForm(true);
+            }}
+          >
+            Analyze
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+
+  //-------------
+
+  // return (
+  //   <div className="sites-list">
+  //     {items.map((item) => {
+  //       const { id, url, scrapeStatus, analyses } = item;
+  //       return (
+  //         <article
+  //           className="sites-item"
+  //           key={id}
+  //           onClick={(e) => {
+  //             //console.log("CLEAR " + statusRefresher);
+  //             // clearInterval(statusRefresher);
+  //             navigate(`/analysis?id=${id}${queries[id] || ""}`);
+  //           }}
+  //         >
+  //           <p className="title" style={{ cursor: "pointer" }}>
+  //             {url}
+  //           </p>
+  //           <div className="btn-container" style={{ display: "flex" }}>
+  //             <button
+  //               type="button"
+  //               className="edit-btn"
+  //               onClick={(e) => {
+  //                 e.stopPropagation();
+  //                 editItem(id);
+  //               }}
+  //             >
+  //               <FaEdit />
+  //             </button>
+  //             <button
+  //               type="button"
+  //               className="delete-btn"
+  //               onClick={(e) => {
+  //                 e.stopPropagation();
+  //                 removeItem(id);
+  //               }}
+  //             >
+  //               <FaTrash />
+  //             </button>
+  //           </div>
+  //           <div
+  //             style={{
+  //               flex: "1 1 100%",
+  //               fontSize: "smaller",
+  //               color: scrapeStatus === "Completed scraping Ok" ? "green" : "red",
+  //             }}
+  //           >
+  //             <b>Status:</b> {scrapeStatus}
+  //           </div>
+  //           <div>
+  //             {analyses.map((analysis) => {
+  //               return (
+  //                 <div style={{ whiteSpace: "pre-line" }} key={analysis.savedAnalysisId}>
+  //                   <p style={{ fontSize: "smaller" }}>
+  //                     <b>Status:</b> {analysis.analysisStatus}
+  //                   </p>
+  //                   {JSON.stringify(analysis.parameters, null, 1)}
+  //                 </div>
+  //               );
+  //             })}
+  //           </div>
+  //         </article>
+  //       );
+  //     })}
+  //   </div>
+  // );
+};
+
+export default MyList;
